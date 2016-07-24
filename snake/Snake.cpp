db5d85a9f9fa2b48d018f935b2330b30136cfc28
Kaadash <arturkudel@gmail.com>
2016-07-24 15:28:56 +0200

    fix rotating

--- snake/Snake.cpp
+++ snake/Snake.cpp
@@ -9,12 +9,11 @@ void Snake::move() {
 		//forward
 		if (this->direction == 0) {
 			newPositionX = this->currentPosition.getX() + 0.01;
-			this->M = glm::translate(this->M, glm::vec3(0.01f, 0, 0));
 		}
 		//backward
 		else {
 			newPositionX = this->currentPosition.getX() - 0.01;
-			this->M = glm::translate(this->M, glm::vec3(-0.01f, 0, 0));
+			//this->M = glm::translate(this->M, glm::vec3(-0.01f, 0, 0));
 		}
 		this->currentPosition.setX(newPositionX);
 	}
@@ -22,14 +21,16 @@ void Snake::move() {
 	else {
 		if (this->direction == 1 || this->direction == -3) {
 			newPositionY = this->currentPosition.getY() - 0.01;
-			this->M = glm::translate(this->M, glm::vec3(0, 0, -0.01));
+			//this->M = glm::translate(this->M, glm::vec3(0, 0, -0.01));
 		}
 		else {
 			newPositionY = this->currentPosition.getY() + 0.01;
-			this->M = glm::translate(this->M, glm::vec3(0, 0, 0.01));
+			//this->M = glm::translate(this->M, glm::vec3(0, 0, 0.01));
 		}
 		this->currentPosition.setY(newPositionY);
 	}
+	this->M = glm::translate(this->M, glm::vec3(0.01f, 0, 0));
+
 }
 
 void Snake::rotate(float degree, float direction) {
@@ -37,13 +38,33 @@ void Snake::rotate(float degree, float direction) {
 	if (this->direction == 4 || this->direction == -4) {
 		this->direction = 0;
 	}
-	//this->M = glm::rotate(this->M, degree, glm::vec3(0, 1.0f, 0));
+	this->M = glm::rotate(this->M, degree, glm::vec3(0, 1.0f, 0));
 }
 
 void Snake::relativeRotate(glm::mat4 *relativeM, float degree) {
 	glm::mat4 previousState = this->M;
 	this->M = glm::rotate(*relativeM, degree, glm::vec3(0, 1.0f, 0));
+	const float PI = 3.141592653589793f;
 	this->M = glm::translate(*relativeM, glm::vec3(this->currentPosition.getX(), 0, this->currentPosition.getY()));
+	if (this->direction % 2 == 0) {
+		//forward
+		if (this->direction == 0) {
+			this->M = glm::rotate(this->M, -PI, glm::vec3(0, 1.0f, 0));
+		}
+		//backward
+		else {
+			this->M = glm::rotate(this->M, PI, glm::vec3(0, 1.0f, 0));
+		}
+	}
+	//moving on axis Y
+	else {
+		if (this->direction == 1 || this->direction == -3) {
+			this->M = glm::rotate(this->M, PI / 2, glm::vec3(0, 1.0f, 0));
+		}
+		else {
+			this->M = glm::rotate(this->M, -PI / 2, glm::vec3(0, 1.0f, 0));
+		}
+	}
 }
 
 
